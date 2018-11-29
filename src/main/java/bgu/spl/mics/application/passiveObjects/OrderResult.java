3f8ed5d76e429c5f913394296301c60d2bab40a4
nivda <nivdaniel99@gmail.com>
2018-11-29 12:15:31 +0200

    test1

--- src/main/java/bgu/spl/mics/application/passiveObjects/OrderResult.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/OrderResult.java
@@ -4,6 +4,7 @@ package bgu.spl.mics.application.passiveObjects;
  * Enum representing result of trying to fetch a book from the inventory.
  */
 public enum OrderResult{
+    //abc tomer
     NOT_IN_STOCK,SUCCESSFULLY_TAKEN
 	//TODO: Implement this
     //aaa
